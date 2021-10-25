83fe6bbd05a1ecc76339aa4d388ab64e07a3b1de
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-10-25 23:51:08 +0800

    Transfer general methods from shuffle to puzzle utility class

---
+++ app/src/main/java/com/gonzales/mark/n_puzzle/PuzzleUtil.kt
@@ -0,0 +1,56 @@
+package com.gonzales.mark.n_puzzle
+
+import java.util.*
+
+class PuzzleUtil {
+    companion object {
+        fun isSolvable(puzzleState: ArrayList<Int>, blankTileMarker: Int): Boolean {
+            return countInversions(puzzleState, blankTileMarker) % 2 == 0
+        }
+
+        private fun countInversions(puzzleState: ArrayList<Int>, blankTileMarker: Int): Int {
+            var numInversions = 0
+
+            for (i in 0 until puzzleState.size - 1) {
+                for (j in i + 1 until puzzleState.size) {
+                    if (!isBlankTile(i, puzzleState, blankTileMarker)
+                        && !isBlankTile(j, puzzleState, blankTileMarker)
+                        && puzzleState[i] > puzzleState[j]
+                    ) {
+                        numInversions++
+                    }
+                }
+            }
+
+            return numInversions
+        }
+
+        fun swapTiles(puzzleState: ArrayList<Int>, blankTileMarker: Int) {
+            var position = 0
+            while (isBlankTile(position, puzzleState, blankTileMarker)
+                || isBlankTile(position + 1, puzzleState, blankTileMarker)
+            ) {
+                position++
+            }
+
+            Collections.swap(puzzleState, position, position + 1)
+        }
+
+        /**
+         * Checks if the tile specified by the given position is a blank tile.
+         *
+         * @param position Position of the tile in the puzzle grid (zero-based, following row-major order).
+         * @param puzzleState Current puzzle state (flattened into one dimension, following row-major order).
+         * @param blankTileMarker Indicator that the tile is blank.
+         * @return <code>true</code> if the tile specified by the given position is a blank tile;
+         * <code>false</code>, otherwise.
+         */
+        private fun isBlankTile(
+            position: Int,
+            puzzleState: ArrayList<Int>,
+            blankTileMarker: Int
+        ): Boolean {
+            return puzzleState[position] == blankTileMarker
+        }
+    }
+}
--- app/src/main/java/com/gonzales/mark/n_puzzle/ShuffleUtil.kt
+++ app/src/main/java/com/gonzales/mark/n_puzzle/ShuffleUtil.kt
@@ -1,8 +1,5 @@
 package com.gonzales.mark.n_puzzle
 
-import androidx.annotation.VisibleForTesting
-import java.util.Collections.swap
-
 class ShuffleUtil {
     companion object {
         fun getValidShuffledState(
@@ -15,70 +12,15 @@ class ShuffleUtil {
                 puzzleState.shuffle()
             }
 
-            ensureSolvability(puzzleState, blankTileMarker)
+            /*
+            * If the 8-puzzle is not solvable (that is, it has an odd number of inversions),
+            * swap a pair of tiles to change the parity and, thus, guarantee solvability.
+            */
+            if (!PuzzleUtil.isSolvable(puzzleState, blankTileMarker)) {
+                PuzzleUtil.swapTiles(puzzleState, blankTileMarker)
+            }
 
             return StatePair(puzzleState, puzzleState.indexOf(blankTileMarker))
-        }
-
-        private fun countInversions(puzzleState: ArrayList<Int>, blankTileMarker: Int): Int {
-            var numInversions = 0
-
-            for (i in 0 until puzzleState.size - 1) {
-                for (j in i + 1 until puzzleState.size) {
-                    if (!isBlankTile(i, puzzleState, blankTileMarker)
-                        && !isBlankTile(j, puzzleState, blankTileMarker)
-                        && puzzleState[i] > puzzleState[j]
-                    ) {
-                        numInversions++
-                    }
-                }
-            }
-
-            return numInversions
-        }
-
-        @VisibleForTesting
-        fun ensureSolvability(puzzleState: ArrayList<Int>, blankTileMarker: Int) {
-            /*
-             * If the 8-puzzle is not solvable (that is, it has an odd number of inversions),
-             * swap a pair of tiles to change the parity and, thus, guarantee solvability.
-             */
-            if (!isSolvable(puzzleState, blankTileMarker)) {
-                swapTiles(puzzleState, blankTileMarker)
-            }
-        }
-
-        @VisibleForTesting
-        fun isSolvable(puzzleState: ArrayList<Int>, blankTileMarker: Int): Boolean {
-            return countInversions(puzzleState, blankTileMarker) % 2 == 0
-        }
-
-        private fun swapTiles(puzzleState: ArrayList<Int>, blankTileMarker: Int) {
-            var position = 0
-            while (isBlankTile(position, puzzleState, blankTileMarker)
-                || isBlankTile(position + 1, puzzleState, blankTileMarker)
-            ) {
-                position++
-            }
-
-            swap(puzzleState, position, position + 1)
-        }
-
-        /**
-         * Checks if the tile specified by the given position is a blank tile.
-         *
-         * @param position Position of the tile in the puzzle grid (zero-based, following row-major order).
-         * @param puzzleState Current puzzle state (flattened into one dimension, following row-major order).
-         * @param blankTileMarker Indicator that the tile is blank.
-         * @return <code>true</code> if the tile specified by the given position is a blank tile;
-         * <code>false</code>, otherwise.
-         */
-        private fun isBlankTile(
-            position: Int,
-            puzzleState: ArrayList<Int>,
-            blankTileMarker: Int
-        ): Boolean {
-            return puzzleState[position] == blankTileMarker
         }
     }
 }
