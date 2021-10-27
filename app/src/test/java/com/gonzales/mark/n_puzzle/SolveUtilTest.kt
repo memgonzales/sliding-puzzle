e09d1ac86024c33b368ae2036d9427bf74c76d8d
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-10-28 00:01:45 +0800

    Add test scenarios for solve utility testing

---
+++ app/src/test/java/com/gonzales/mark/n_puzzle/SolveUtilTest.kt
@@ -0,0 +1,49 @@
+package com.gonzales.mark.n_puzzle
+
+import org.junit.Test
+
+class SolveUtilTest {
+    companion object {
+        private const val NUM_TESTS = 1000
+    }
+
+    @Test
+    fun `Check if the goal state is reached for solvable puzzles`() {
+
+    }
+
+    @Test
+    fun `Check if the goal state is not reached for unsolvable puzzles`() {
+
+    }
+
+    @Test
+    fun `Check if the sequence of moves returned obeys the tile sliding rules`() {
+
+    }
+
+    @Test
+    fun `Check if the goal state is reached in 0 moves if the current state is the goal state itself`() {
+
+    }
+
+    @Test
+    fun `Check if the goal state is reached in 1 move if the current state is reached by sliding the tile beside the blank tile to the right`() {
+
+    }
+
+    @Test
+    fun `Check if the goal state is reached in 1 move if the current state is reached by sliding the tile above the blank tile down`() {
+
+    }
+
+    @Test
+    fun `Check if the goal state is reached in 31 moves if the current state is one of the two states that require the most number of moves`() {
+
+    }
+
+    @Test
+    fun `Check if the goal state is reached in 31 moves if the current state is the other of the two states that require the most number of moves`() {
+
+    }
+}
