fc499a0bc7628f8cc921cf183df237c1357a80ea
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-10-27 18:29:55 +0800

    Add unit tests for solvable and unsolvable puzzles

--- app/src/test/java/com/gonzales/mark/n_puzzle/PuzzleUtilTest.kt
+++ app/src/test/java/com/gonzales/mark/n_puzzle/PuzzleUtilTest.kt
@@ -1,10 +1,34 @@
 package com.gonzales.mark.n_puzzle
 
 import com.gonzales.mark.n_puzzle.TestUtil.Companion.permute
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Test
 
 class PuzzleUtilTest {
+    @Test
+    fun `Check if a shuffled 8-puzzle with solution is detected as solvable`() {
+        val puzzleState: ArrayList<Int> = arrayListOf(0, 1, 2, 3, 4, 8, 6, 7, 5)
+        val errorMessage = "\nShould be solvable: $puzzleState\n"
+
+        assertTrue(errorMessage, PuzzleUtil.isSolvable(puzzleState, TestUtil.BLANK_TILE_MARKER))
+    }
+
+    @Test
+    fun `Check if an already-solved 8-puzzle is detected as solvable`() {
+        val puzzleState: ArrayList<Int> = arrayListOf(0, 1, 2, 3, 4, 5, 6, 7, 8)
+        val errorMessage = "\nShould be solvable: $puzzleState\n"
+
+        assertTrue(errorMessage, PuzzleUtil.isSolvable(puzzleState, TestUtil.BLANK_TILE_MARKER))
+    }
+
+    @Test
+    fun `Check if an 8-puzzle with no solution is detected as unsolvable`() {
+        val puzzleState: ArrayList<Int> = arrayListOf(0, 1, 2, 3, 4, 5, 7, 6, 8)
+        val errorMessage = "\nShould not be solvable: $puzzleState\n"
+
+        assertFalse(errorMessage, PuzzleUtil.isSolvable(puzzleState, TestUtil.BLANK_TILE_MARKER))
+    }
+
     @Test
     fun `Check if the number of solvable 8-puzzles is equal to half of the total number of puzzle configurations`() {
         val puzzleState: ArrayList<Int> = arrayListOf(0, 1, 2, 3, 4, 5, 6, 7, 8)
