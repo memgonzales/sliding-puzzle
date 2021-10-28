b61f92bc1882bb6273ac02eef68cf1603dde5aa1
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-10-28 08:37:37 +0800

    Add scenarios for unit testing the node data class

---
+++ app/src/test/java/com/gonzales/mark/n_puzzle/NodeTest.kt
@@ -0,0 +1,35 @@
+package com.gonzales.mark.n_puzzle
+
+import org.junit.Test
+
+class NodeTest {
+    @Test
+    fun `Check if the puzzle state hash correctly reflects the puzzle state`() {
+
+    }
+
+    @Test
+    fun `Check if the f-value is computed as the sum of the g- and h-values`() {
+
+    }
+
+    @Test
+    fun `Check if nodes are equal if their puzzle states are equal`() {
+
+    }
+
+    @Test
+    fun `Check if nodes are unequal if their puzzle states are unequal`() {
+
+    }
+
+    @Test
+    fun `Check if the node hash reflects the puzzle state`() {
+
+    }
+
+    @Test
+    fun `Check if the node hash depends only on the puzzle state`() {
+
+    }
+}
