26e2417753c1f9f49ec9d3422b8abb891d3e5cb5
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-10-28 08:17:31 +0800

    Add scenarios for unit testing time utility class

---
+++ app/src/test/java/com/gonzales/mark/n_puzzle/TimeUtilTest.kt
@@ -0,0 +1,55 @@
+package com.gonzales.mark.n_puzzle
+
+import org.junit.Test
+
+class TimeUtilTest {
+    @Test
+    fun `Check if display is 0 00 00 if time is 0 seconds`() {
+
+    }
+
+    @Test
+    fun `Check if display is of the form 0 00 ss if time is below a minute but greater than 0 seconds`() {
+
+    }
+
+    @Test
+    fun `Check if display is of the form 0 01 00 if time is equal to a minute`() {
+
+    }
+
+    @Test
+    fun `Check if display is of the form 0 mm 00 if time is below an hour and is an exact multiple of a minute`() {
+
+    }
+
+    @Test
+    fun `Check if display is of the form 0 mm ss if time is below an hour and is not an exact multiple of a minute`() {
+
+    }
+
+    @Test
+    fun `Check if display is of the form 1 00 00 if time is equal to an hour`() {
+
+    }
+
+    @Test
+    fun `Check if display is of the form h 00 ss if time converts to a multiple of an hour and some seconds`() {
+
+    }
+
+    @Test
+    fun `Check if display is of the form h mm 00 if time converts to a multiple of an hour and some minutes`() {
+
+    }
+
+    @Test
+    fun `Check if display is of the form h mm ss if time converts to some hours, minutes, and seconds`() {
+
+    }
+
+    @Test
+    fun `Check if the largest number of seconds handled by the system can be properly displayed`() {
+
+    }
+}
