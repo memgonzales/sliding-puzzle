4bf6fd8625ea822558683bd44fff6fe216b2de2b
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2022-02-27 21:36:25 +0800

    Add layout for splash screen

---
+++ app/src/main/java/com/gonzales/mark/n_puzzle/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.gonzales.mark.n_puzzle
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
